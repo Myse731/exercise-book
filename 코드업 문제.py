7b61d148a3cd10ccb3df41e4c09539880c509633
Myse731 <minsekwon731@gmail.com>
2026-05-26 21:58:58 +0900

    문제풀이

---
+++ 코드업 문제.py
@@ -0,0 +1,40 @@
+#1351번 구구단 출력하기 2 
+# a, b = map(int, input().split())
+
+# for i in range(a, b+1):
+# 	for j in range(1, 10):
+# 		print(f'{i} * {j} = {i * j}')
+
+#1352번 사각형 출력하기 1
+# a = int(input())
+# for i in range(a):
+# 	for j in range(a):
+# 		print("*", end = '')
+# 	print("")
+
+#1353번 삼각형 출력하기1
+# a = int(input())
+
+# for i in range(a):
+# 	for j in range(i+1):
+# 		print("*", end = '')
+# 	print("")
+
+#1354번 삼각형 출력하기2
+# a = int(input())
+
+# for i in range(a):
+# 	for j in range(a-i):
+# 		print("*", end = "")
+# 	print("")
+
+#1355번 삼각형 출력하기3
+# a = int(input())
+
+# for i in range(a):
+# 	for j in range(i):
+# 		print(" ", end = "")
+# 	for h in range(a - i):
+# 		print("*",end = "")
+# 	print("")
+
