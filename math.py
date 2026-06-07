aabf02048804957b9dddca945643cd60ca6fcd92
Myse731 <minsekwon731@gmail.com>
2026-06-07 14:42:22 +0900

    순열, 조합

--- math.py
+++ math.py
@@ -1,145 +1,100 @@
-# def get_p(lists):
-#   result = []
-#   if len(lists) == 1:
-#     return [lists]
-    
-#   for i in range(len(lists)):
-#     g = lists[i]
-#     n = lists[:i] + lists[i+1:]
-     
-#     zip = get_p(n)
-
-#     for j in zip:
-#       result.append([lists[i]]+j)
-
-#   return result
-
-# def get_c(lists, r):
-#   if r == 0:
-#     return [[]]
-#   if len(lists) == 0:
-#     return []
-#   result = []
-#   for i in range(len(lists)):
-#     s = lists[i]
-#     n = lists[i+1:]
-
-#     cc = get_c(n, r-1)
-
-#     for j in cc:
-#       result.append([s]+j)
-
-#   return result
-
-## A번 1번
+# 순열 코드
 # def get_p(lists):
 #     result = []
 #     if len(lists) == 1:
 #         return [lists]
+#     else:
+#         for i in range(len(lists)):
+#             s = lists[i]
+#             n = lists[:i] + lists[i+1:]
 
-#     for i in range(len(lists)):
-#         n = lists[:i] + lists[i+1:]
+#             hap = get_p(n)
 
-#         zip = get_p(n)
-
-#         for j in zip:
-#             result.append([lists[i]] + j)
-
+#             for j in hap:
+#                 result.append([s]+j)
+    
 #     return result
 
 
-# perms = get_p("a b c d e".split())
+# A문제 1번
+# lists = list(map(str, input().split()))
 
-# count = 0
+# def get_ea(lists):
+#     result1 = get_p(lists)
+#     result2 = []
+#     for rows in result1:
+#         new_row = []
 
-# for p in perms:
-#     a = p.index('a')
-#     e = p.index('e')
+#         for words in rows:
+#             if words =='ae':
+#                 words = 'ea'
+#                 new_row.append(words)
+#             else:
+#                 new_row.append(words)
+#         result2.append(new_row)
 
-#     if abs(a - e) == 1:
-#         count += 1
+#     return result1 + result2
 
-# print("경우의 수:", count)
+# print(len(get_ea(lists)))
 
-## 2번
-# perms = get_p("a b c d e".split())
+#A문제 2번
+# def get_zamo(li1, li2):
+#     result = []
+#     o_p = get_p(li1)
+#     t_p = get_p(li2)
 
-# count = 0
-# vowel = ['a', 'e']
+#     for i in o_p:
+#         for j in t_p:
+#             result.append([i[0],j[0],i[1],j[1],i[2]])
+#     return result
 
-# for p in perms:
-#     if (p[0] not in vowel and
-#         p[1] in vowel and
-#         p[2] not in vowel and
-#         p[3] in vowel and
-#         p[4] not in vowel):
-#         count += 1
+# consonants = ['b', 'c', 'd']
+# vowels = ['a', 'e']
 
-# print("경우의 수:", count)
+# print(len(get_zamo(consonants, vowels)))
 
-##B번 1번
 # def get_c(lists, r):
-#   result = []
-#   if r == 0:
-#     return [[]]
-#   if len(lists) == 0:
-#     return []
-#   for i in range(len(lists)):
-#     s = lists[i]
-#     n = lists[i+1:]
+#     result = []
+#     if r == 1:
+#         return [[i] for i in lists]
+    
+#     for i in range(len(lists)):
+#         s = lists[i]
 
-#     cc = get_c(n, r-1)
+#         n = lists[i+1:]
+#         hap = get_c(n, r-1)
 
-#     for j in cc:
-#       result.append([s]+j)
-
-#   return result
-
-# cards = [1,2,3,4,5,6,7,8,9]
-
-# combs = get_c(cards, 3)
-
-# count = 0
-
-# for c in combs:
-#     if 7 in c:
-#         count += 1
-
-# print(count)
-
-##B번 2번
-# def get_c(lists, r):
-#   result = []
-#   if r == 0:
-#     return [[]]
-#   if len(lists) == 0:
-#     return []
-#   for i in range(len(lists)):
-#     s = lists[i]
-#     n = lists[i+1:]
-
-#     cc = get_c(n, r-1)
-
-#     for j in cc:
-#       result.append([s]+j)
-
-#   return result
+#         for j in hap:
+#             result.append([s]+j)
+#     return result
 
 # cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# combs = get_c(cards, 3)
 
-# count = 0
+#B문제 1번
+# def get_s(lists, r):
+#     result1 = []
+#     s = 7
+#     lists.remove(7)
+#     result2 = get_c(lists, r-1)
+#     for i in result2:
+#         result1.append([s]+i)
 
-# for c in combs:
-#   even = 0
-#   odd = 0
+#     return result1
 
-#   for num in c:
-#     if num % 2 == 0:
-#       even += 1
-#     else :
-#       odd += 1
-#   if even == 2 and odd == 1:
-#     count += 1
+# print(len(get_s(cards, 3)))
 
-# print(count)
+#B문제 2번
+# def get_hz(lists):
+#     hol = [x for x in lists if x % 2 != 0]
+#     zzak = [x for x in lists if x % 2 == 0]
+
+#     hol_c = get_c(hol, 1)
+#     zzak_c = get_c(zzak, 2)
+
+#     result = []
+#     for i in hol_c:
+#         for j in zzak_c:
+#             result.append(i+j)
+#     return result
+
+# print(len(get_hz(cards)))
