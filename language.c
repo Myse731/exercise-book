bee8f172a9c56aa427f68e741e005b3ccb4f8213
Myse731 <minsekwon731@gmail.com>
2026-07-05 23:58:09 +0900

    c

--- language.c
+++ language.c
@@ -588,3 +588,38 @@
 // 	}
 // 	return odd_sum(n-1);
 // }
+
+#include <stdio.h>
+#include <stdlib.h>
+void print_array(int **arr, int n) {
+	int i, j;
+	for(i = 0; i < n; i++) {
+		for(j = 0; j < n; j++) {
+			printf("%5d", arr[i][j]);
+		}
+		printf("\n");
+	}	
+}
+int main() {
+int n;
+scanf("%d", &n);
+int **arr = (int**)malloc(sizeof(int *) * n);
+for(int i = 0; i < n; i++){
+	arr[i] = (int *)malloc(sizeof(int) * n);
+}
+
+int add = 1;
+for(int i = 0; i < n; i++){
+	if(i % 2 != 0){
+		for(int j = n - 1; j >= 0; j--){
+			arr[j][i] = add++;
+		}
+	}
+	else{
+		for(int j = 0; j < n; j++){
+			arr[j][i] = add++;
+		}
+	}
+}
+	print_array(arr, n);
+}
