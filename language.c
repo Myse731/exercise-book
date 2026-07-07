a47e874d2dbdd3c72216757dd215f73737e898eb
Myse731 <minsekwon731@gmail.com>
2026-07-07 09:26:58 +0900

    c

--- language.c
+++ language.c
@@ -589,37 +589,101 @@
 // 	return odd_sum(n-1);
 // }
 
-#include <stdio.h>
-#include <stdlib.h>
-void print_array(int **arr, int n) {
-	int i, j;
-	for(i = 0; i < n; i++) {
-		for(j = 0; j < n; j++) {
-			printf("%5d", arr[i][j]);
-		}
-		printf("\n");
-	}	
-}
-int main() {
-int n;
-scanf("%d", &n);
-int **arr = (int**)malloc(sizeof(int *) * n);
-for(int i = 0; i < n; i++){
-	arr[i] = (int *)malloc(sizeof(int) * n);
-}
+// #include <stdio.h>
+// #include <stdlib.h>
+// void print_array(int **arr, int n) {
+// 	int i, j;
+// 	for(i = 0; i < n; i++) {
+// 		for(j = 0; j < n; j++) {
+// 			printf("%5d", arr[i][j]);
+// 		}
+// 		printf("\n");
+// 	}	
+// }
+// int main() {
+// int n;
+// scanf("%d", &n);
+// int **arr = (int**)malloc(sizeof(int *) * n);
+// for(int i = 0; i < n; i++){
+// 	arr[i] = (int *)malloc(sizeof(int) * n);
+// }
 
-int add = 1;
-for(int i = 0; i < n; i++){
-	if(i % 2 != 0){
-		for(int j = n - 1; j >= 0; j--){
-			arr[j][i] = add++;
-		}
-	}
-	else{
-		for(int j = 0; j < n; j++){
-			arr[j][i] = add++;
-		}
-	}
-}
-	print_array(arr, n);
-}
+// int add = 1;
+// for(int i = 0; i < n; i++){
+// 	if(i % 2 != 0){
+// 		for(int j = n - 1; j >= 0; j--){
+// 			arr[j][i] = add++;
+// 		}
+// 	}
+// 	else{
+// 		for(int j = 0; j < n; j++){
+// 			arr[j][i] = add++;
+// 		}
+// 	}
+// }
+// 	print_array(arr, n);
+// }
+
+// #include <stdio.h>
+// #define PI 3.14159
+// #define LIMIT 100.0
+// #define MSG "passed!"
+// #define ERR_PRN printf("Exceed limits\n")
+
+// int main(){
+//     double radius, area;
+
+//     printf("input radius : ");
+//     scanf("%lf", &radius);
+//     area = PI * radius * radius;
+//     if(area > LIMIT) ERR_PRN;
+//     else printf("circle area : %.2lf (%s)\n", area, MSG);
+// }
+
+// #include <stdio.h>
+// #define MUL(a, b) (a * b)
+// #define SUB(a, b) (a - b)
+// #define SWAP(a, b) {int temp = a; a = b; b = temp;}
+
+// int main(void){
+//     int a = 20, b = 10;
+//     printf("Sub : %d\n", SUB(a, b));
+//     printf("Product: %d\n", 400/MUL(a, b));
+//     SWAP(a, b);
+//     printf("swap_Sub : %d\n", SUB(a, b));
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// struct student{
+//     int num;
+//     char name[20];
+//     double grade;
+// };
+
+// int main(){
+//     struct student s1;
+//     scanf("%d", &s1.num);
+//     scanf("%s", s1.name);
+//     scanf("%lf", &s1.grade);
+//     printf("num : %d\n", s1.num);
+//     printf("name : %s\n", s1.name);
+//     printf("grade : %.1lf\n", s1.grade);
+// }
+
+// #include <stdio.h>
+//  struct student {
+//     char a;
+//     long long b;
+//     char c;
+//  };
+
+// int main(){
+//     struct student s1;
+//     printf("%d\n", sizeof(s1.a));
+//     printf("%d\n", sizeof(s1.b));
+//     printf("%d\n", sizeof(s1.c));
+//     printf("%d", sizeof(s1));
+//     return 0;
+// }
