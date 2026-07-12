7d786b0287abb957971df5485006daae99e88835
Myse731 <minsekwon731@gmail.com>
2026-07-12 23:17:25 +0900

    수정

--- language.c
+++ language.c
@@ -143,35 +143,36 @@
 
 // #include <stdio.h>
 // int main(void){
-// 	printf("Hello\n");
-// 	printf("World");
-// 	return 0;
+//  printf("Hello\n");
+//  printf("World");
+//  return 0;
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	printf(" \'Hello World\" ");
+//  printf(" \'Hello World\" ");
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	long long a;
-// 	double b;
-// 	char c;
-// 	scanf("%lld %lf %c", &a,&b,&c);
-// 	printf("%lld %.10lf %c", a, b, c);
-// 	return 0;
+//  long long a;
+//  double b;
+//  char c;
+//  scanf("%lld %lf %c", &a,&b,&c);
+//  printf("%lld %.10lf %c", a, b, c);
+//  return 0;
 // }
 
 // #include <stdio.h>
 // #include <stdbool.h>
 // int main() {
-// 	int a, b;
-// 	scanf("%d %d", &a, &b);
-// 	printf("%d\n%d\n%d", a&&b, a||b, !a);
-// 	return 0;
+//  int a, b;
+//  scanf("%d %d", &a, &b);
+//  printf("%d\n%d\n%d", a&&b, a||b, !a);
+//  return 0;
 // }
 
+// [중복 통합] 문자열의 공백을 제외하고 출력하는 로직 (기존 3개 중 1개만 보존)
 // #include <stdio.h>
 // int main(void){
 //     char arr[101];
@@ -185,27 +186,27 @@
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
-// 	scanf("%d", &n);
-// 	int called[24] = {0};
-// 	int count[24] = {0};
-// 	for(int i = 0; i < n; i++){
-// 		scanf("%d", &called[i]);
-// 	}
-// 	for(int j = 0; j < n; j++){
-// 		int number = called[j];
+//  int n;
+//  scanf("%d", &n);
+//  int called[24] = {0};
+//  int count[24] = {0};
+//  for(int i = 0; i < n; i++){
+//      scanf("%d", &called[i]);
+//  }
+//  for(int j = 0; j < n; j++){
+//      int number = called[j];
 //         if (number != 0) {
-// 		count[number - 1] += 1;
+//      count[number - 1] += 1;
 //         }
-// 	}
-// 	for(int h = 0; h < 23; h++){
-// 		printf("%d ", count[h]);
-// 	}
+//  }
+//  for(int h = 0; h < 23; h++){
+//      printf("%d ", count[h]);
+//  }
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
+//  int n;
 //     scanf("%d", &n);
 //     int arr[n];
 //     for(int i = 0; i < n; i++){
@@ -240,17 +241,6 @@
 
 // #include <stdio.h>
 // int main(void){
-//     char arr[101];
-//     fgets(arr, 100, stdin);
-//     for(int i = 0; arr[i] != '\0'; i++){
-//         if(arr[i] != ' '){
-//             printf("%c", arr[i]);
-//         }
-//     }
-// }
-
-// #include <stdio.h>
-// int main(void){
 //     int n;
 //     scanf("%d", &n);
 //     char name[n];
@@ -269,194 +259,181 @@
 //         score[maxidx] = 0;
 //         printf("%s", name[maxidx]);
 //     }
-   
-// }
-
-
-// #include <stdio.h>
-// int main(void){
-// 	int n;
-// 	scanf("%d", &n);
-// 	int arr[n];
-// 	for(int i = 0; i < n; i++){
-// 		scanf("%d", &arr[i]);
-// 	}
-// 	for(int i = n - 1; i >= 0; i--){
-// 		printf("%d ", arr[i]);
-// 	}
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
-// 	scanf("%d", &n);
-// 	int arr[n];
-// 	for(int i = 0; i < n; i++){
-// 		scanf("%d", &arr[i]);
-// 	}
-// 	for(int j = 0; j < n; j++){
-// 		for(int k = j; k < n; k++){
-// 			printf("%d ", arr[k]);
-// 		}
-// 		for(int h = 0; h < j; h++){
-// 			printf("%d ", arr[h]);
-// 		}
+//  int n;
+//  scanf("%d", &n);
+//  int arr[n];
+//  for(int i = 0; i < n; i++){
+//      scanf("%d", &arr[i]);
+//  }
+//  for(int i = n - 1; i >= 0; i--){
+//      printf("%d ", arr[i]);
+//  }
+// }
+
+// #include <stdio.h>
+// int main(void){
+//  int n;
+//  scanf("%d", &n);
+//  int arr[n];
+//  for(int i = 0; i < n; i++){
+//      scanf("%d", &arr[i]);
+//  }
+//  for(int j = 0; j < n; j++){
+//      for(int k = j; k < n; k++){
+//          printf("%d ", arr[k]);
+//      }
+//      for(int h = 0; h < j; h++){
+//          printf("%d ", arr[h]);
+//      }
 //         printf("\n");
-// 	}
+//  }
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	char arr[101];
-// 	fgets(arr, sizeof(arr), stdin);
-// 	for(int i = 0; arr[i] != '\0'; i++){
-// 		if(arr[i] != ' '){
-// 			printf("%c", arr[i]);
-// 		}
-// 	}
+//  char a[100000];
+//  scanf("%s", a);
+//  int sumr = 0, suml = 0;
+//  for(int i = 0; a[i] != '\0'; i++){
+//      if(a[i] == '('){
+//          suml += 1;
+//      }
+//      else if(a[i] == ')'){
+//          sumr += 1;
+//      }
+//  }
+//  printf("%d %d", suml, sumr);
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	char a[100000];
-// 	scanf("%s", a);
-// 	int sumr = 0, suml = 0;
-// 	for(int i = 0; a[i] != '\0'; i++){
-// 		if(a[i] == '('){
-// 			suml += 1;
-// 		}
-// 		else if(a[i] == ')'){
-// 			sumr += 1;
-// 		}
-// 	}
-// 	printf("%d %d", suml, sumr);
+//  int n;
+//  scanf("%d", &n);
+//  int cards[n-1];
+//  int sum1 = 0;
+//  int sum2 = 0;
+//  for(int i = 0; i < n - 1; i++){
+//      scanf("%d", &cards[i]);
+//  }
+//  for(int j = 0; j < n - 1; j++){
+//      sum1 += cards[j];
+//  }
+//  for(int k = 1; k < n+1; k++){
+//      sum2 += k;
+//  }
+//  int sum3 = sum2 - sum1;
+//  printf("%d", sum3);
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
-// 	scanf("%d", &n);
-// 	int cards[n-1];
-// 	int sum1 = 0;
-// 	int sum2 = 0;
-// 	for(int i = 0; i < n - 1; i++){
-// 		scanf("%d", &cards[i]);
-// 	}
-// 	for(int j = 0; j < n - 1; j++){
-// 		sum1 += cards[j];
-// 	}
-// 	for(int k = 1; k < n+1; k++){
-// 		sum2 += k;
-// 	}
-// 	int sum3 = sum2 - sum1;
-// 	printf("%d", sum3);
-// }
-
-// #include <stdio.h>
-// int main(void){
-// 	int n;
-// 	scanf("%d", &n);
-// 	int arr[1000];
-// 	for(int i = 0; i < n; i++){
-// 		scanf("%d", &arr[i]);
-// 	}
-// 	for(int j = 0; j < n; j++){
+//  int n;
+//  scanf("%d", &n);
+//  int arr[1000];
+//  for(int i = 0; i < n; i++){
+//      scanf("%d", &arr[i]);
+//  }
+//  for(int j = 0; j < n; j++){
 //         printf("%d: ", j+1);
-// 		for(int k = 0; k < n; k++){
-// 			if(k == j){
-// 				continue;
-// 			}
-// 			if(arr[j] > arr[k]){
-// 				printf("> ");
-// 			}
-// 			else if(arr[j] < arr[k]){
-// 				printf("< ");
-// 			}
-// 			else if(arr[j] == arr[k]){
-// 				printf("= ");
-// 			}
-// 		}
+//      for(int k = 0; k < n; k++){
+//          if(k == j){
+//              continue;
+//          }
+//          if(arr[j] > arr[k]){
+//              printf("> ");
+//          }
+//          else if(arr[j] < arr[k]){
+//              printf("< ");
+//          }
+//          else if(arr[j] == arr[k]){
+//              printf("= ");
+//          }
+//      }
 //         printf("\n");
-// 	}
+//  }
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	char arr[100];
-// 	scanf("%s", arr);
-// 	for(int i = 0; arr[i] != '\0'; i++){
-// 		if(arr[i] >= 65 && arr[i] <= 90){
-// 			arr[i] += 32;
-// 		}
-// 		else if(arr[i] >= 97 && arr[i] <= 122){
-// 			arr[i] -= 32;
-// 		}
-// 	}
-// 	printf("%s", arr);
+//  char arr[100];
+//  scanf("%s", arr);
+//  for(int i = 0; arr[i] != '\0'; i++){
+//      if(arr[i] >= 65 && arr[i] <= 90){
+//          arr[i] += 32;
+//      }
+//      else if(arr[i] >= 97 && arr[i] <= 122){
+//          arr[i] -= 32;
+//      }
+//  }
+//  printf("%s", arr);
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	char word[16];
-// 	scanf("%s", word);
-// 	if(word == 'love'){
-// 		printf("I Love you");
-// 	}
-// 	else{
-// 		printf("");
-// 	}
+//  char word[16];
+//  scanf("%s", word);
+//  if(word == 'love'){
+//      printf("I Love you");
+//  }
+//  else{
+//      printf("");
+//  }
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
-// 	int add = 1;
-// 	scanf("%d", &n);
-// 	int arr[n][n];
-// 	for(int i = 0; i < n; i++){
-// 		for(int j = 0; j < n; j++){
-// 			arr[i][j] = add++;
-// 		}
-// 	}
-// 	for(int k = 0; k < n; k++){
-// 		for(int h = 0; h < n; h++){
-// 			printf("%d ", arr[k][h]);
-// 		}
-// 		printf("\n");
-// 	}
+//  int n;
+//  int add = 1;
+//  scanf("%d", &n);
+//  int arr[n][n];
+//  for(int i = 0; i < n; i++){
+//      for(int j = 0; j < n; j++){
+//          arr[i][j] = add++;
+//      }
+//  }
+//  for(int k = 0; k < n; k++){
+//      for(int h = 0; h < n; h++){
+//          printf("%d ", arr[k][h]);
+//      }
+//      printf("\n");
+//  }
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
-// 	scanf("%d", &n);
-// 	int add = 1;
-// 	int arr[100][100];
-// 	for(int i = 0; i < n; i++){
-// 		if(i % 2 == 0){
-// 			for(int j = 0; j < n; j++){
-// 				arr[i][j] = add++;
-// 			}
-// 		}
+//  int n;
+//  scanf("%d", &n);
+//  int add = 1;
+//  int arr[100][100];
+//  for(int i = 0; i < n; i++){
+//      if(i % 2 == 0){
+//          for(int j = 0; j < n; j++){
+//              arr[i][j] = add++;
+//          }
+//      }
 
-// 		else{
-// 			for(int j = n - 1; j >= 0; j--){
-// 				arr[i][j] = add++;
-// 			}
-// 		}
-// 	}
-// 	for(int i = 0; i < n; i++){
-// 		for(int j = 0; j < n; j++){
-// 			printf("%d ", arr[i][j]);
-// 		}
-// 		printf("\n");
-// 	}
+//      else{
+//          for(int j = n - 1; j >= 0; j--){
+//              arr[i][j] = add++;
+//          }
+//      }
+//  }
+//  for(int i = 0; i < n; i++){
+//      for(int j = 0; j < n; j++){
+//          printf("%d ", arr[i][j]);
+//      }
+//      printf("\n");
+//  }
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	int n;
-// 	scanf("%d", &n);
+//  int n;
+//  scanf("%d", &n);
 //     int arr[n];
 //     for(int i = 0; i < n; i++){
 //         scanf("%d", &arr[i]);
@@ -548,23 +525,23 @@
 
 // #include <stdio.h>
 // int main(void){
-// 	char words[9];
-// 	scanf("%s", words);
-// 	printf("%s", words);
+//  char words[9];
+//  scanf("%s", words);
+//  printf("%s", words);
 // }
 
 // #include <stdio.h>
 // int main(void){
-// 	char words[51];
-// 	fgets(words, 51, stdin);
-// 	int count = 0;
-// 	for(int i = 0; words[i] != '\0'; i++){
-// 		if((words[i] >= 'A' && words[i] <= 'Z') || (words[i] >= 'a' && words[i] <= 'z')){
-// 			continue;
-// 		}
-// 		count += 1;
-// 	}
-// 	printf("%d", count);
+//  char words[51];
+//  fgets(words, 51, stdin);
+//  int count = 0;
+//  for(int i = 0; words[i] != '\0'; i++){
+//      if((words[i] >= 'A' && words[i] <= 'Z') || (words[i] >= 'a' && words[i] <= 'z')){
+//          continue;
+//      }
+//      count += 1;
+//  }
+//  printf("%d", count);
 // }
 
 
@@ -573,55 +550,55 @@
 // int odd_sum(int n);
 
 // int main() {
-// 	int n, i;
-// 	scanf("%d", &n);
-// 	printf("%d", odd_sum(n));
+//  int n, i;
+//  scanf("%d", &n);
+//  printf("%d", odd_sum(n));
 // }
 
 // int odd_sum(int n){
-// 	int sum = 0;
-// 	if(n == 1){
-// 		return 1;
-// 	}
-// 	else if(n % 2 != 0){
-// 		return n + odd_sum(n-1);
-// 	}
-// 	return odd_sum(n-1);
+//  int sum = 0;
+//  if(n == 1){
+//      return 1;
+//  }
+//  else if(n % 2 != 0){
+//      return n + odd_sum(n-1);
+//  }
+//  return odd_sum(n-1);
 // }
 
 // #include <stdio.h>
 // #include <stdlib.h>
 // void print_array(int **arr, int n) {
-// 	int i, j;
-// 	for(i = 0; i < n; i++) {
-// 		for(j = 0; j < n; j++) {
-// 			printf("%5d", arr[i][j]);
-// 		}
-// 		printf("\n");
-// 	}	
+//  int i, j;
+//  for(i = 0; i < n; i++) {
+//      for(j = 0; j < n; j++) {
+//          printf("%5d", arr[i][j]);
+//      }
+//      printf("\n");
+//  }   
 // }
 // int main() {
 // int n;
 // scanf("%d", &n);
 // int **arr = (int**)malloc(sizeof(int *) * n);
 // for(int i = 0; i < n; i++){
-// 	arr[i] = (int *)malloc(sizeof(int) * n);
+//  arr[i] = (int *)malloc(sizeof(int) * n);
 // }
 
 // int add = 1;
 // for(int i = 0; i < n; i++){
-// 	if(i % 2 != 0){
-// 		for(int j = n - 1; j >= 0; j--){
-// 			arr[j][i] = add++;
-// 		}
-// 	}
-// 	else{
-// 		for(int j = 0; j < n; j++){
-// 			arr[j][i] = add++;
-// 		}
-// 	}
+//  if(i % 2 != 0){
+//      for(int j = n - 1; j >= 0; j--){
+//          arr[j][i] = add++;
+//      }
+//  }
+//  else{
+//      for(int j = 0; j < n; j++){
+//          arr[j][i] = add++;
+//      }
+//  }
 // }
-// 	print_array(arr, n);
+//  print_array(arr, n);
 // }
 
 // #include <stdio.h>
@@ -690,7 +667,31 @@
 
 // #include <stdio.h>
 // int main(void){
-// 	int a, b;
-// 	scanf("%d %d", &a, &b);
-// 	printf("%d", a > b ? a : b);
+//  int a, b;
+//  scanf("%d %d", &a, &b);
+//  printf("%d", a > b ? a : b);
+// }
+
+// #include <stdio.h>
+// int main(void){
+//  int n;
+//  int max = 0;
+//  int max2 = 0;
+//  scanf("%d", &n);
+//  int arr[n];
+//  int copy[101] = {0};
+//  for(int i = 0; i < n; i++){
+//      scanf("%d", &arr[i]);
+//  }
+//  for(int j = 0; j < n; j++){
+//      copy[arr[j]] += 1;
+//  }
+//  max = copy[0];
+//  for(int h = 0; h < 100; h++){
+//      if(copy[h] > max){
+//          max = copy[h];
+//          max2 = h;
+//      }
+//  }
+//  printf("%d", max2);
 // }
