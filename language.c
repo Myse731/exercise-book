82fd40cddd21a47a249f98c1f6303ccb336ac372
Myse731 <minsekwon731@gmail.com>
2026-07-17 23:58:23 +0900

    c

--- language.c
+++ language.c
@@ -695,3 +695,20 @@
 //  }
 //  printf("%d", max2);
 // }
+// #include <stdio.h>
+// int main(void){
+// 	int a;
+// 	scanf("%d", &a);
+// 	if(a > 0){
+// 		printf("plus\n");
+// 	}
+// 	else{
+// 		printf("minus\n");
+// 	}
+// 	if(a % 2 == 0){
+// 		printf("even");
+// 	}
+// 	else{
+// 		printf("odd");
+// 	}
+// }
